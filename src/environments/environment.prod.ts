2ea96a0c7188717fe57282a3419c5159be3c8c83
Dmytro Putrin <moncodp@gmail.com>
2019-01-29 16:59:15 +0200

    [Firebase setup]

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5GfB5XTGbUG_Yl9MKP_Krw0Y_qv4N4tI',
+    authDomain: 'aspasskaya-82277.firebaseapp.com',
+    databaseURL: 'https://aspasskaya-82277.firebaseio.com',
+    projectId: 'aspasskaya-82277',
+    storageBucket: 'aspasskaya-82277.appspot.com',
+    messagingSenderId: '38612416791'
+  }
 };
